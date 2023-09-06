ffeac6e6684b995e2ee3b94f9238c9bde357c77e
holly <h.person.notrobot@gmail.com>
2023-09-06 16:04:44 +0100

    Implemented Agent and Network classes, and fixed broken window rendering

--- main.cu
+++ main.cu
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <thread>
+#include <vector>
+
 
 #define max_prey 100
 #define max_predators 50
+#define initial_prey 50
+#define initial_predators 25
 #define map_size_x 500
 #define map_size_y 500
 #define prey_max_speed 1
@@ -12,15 +16,69 @@
 #define predators_max_speed 1
 #define predators_max_turn_speed 1
 
+#define input_number 20
+#define internal_number 5
+#define output_number 2
+
+
 using namespace std::literals;
 using clock_type = std::chrono::high_resolution_clock;
+
+
+
+class Network{
+private:
+    float input_to_internal_weight[internal_number][input_number];
+    float internal_bias[internal_number];
+
+    float internal_to_output_weight[output_number][internal_number];
+    float output_bias[output_number];
+
+    int internals[internal_number];
+public:
+    int inputs[input_number];
+    int outputs[output_number];
+
+
+    void think(){
+        for (int n = 0; n < internal_number; n++){ // Input -> internals
+            internals[n] = internal_bias[n];
+            for (int i = 0; i < input_number; i++){
+                internals[n] += inputs[i]*input_to_internal_weight[n][i]; // This feels like it might be the wrong way round
+            }
+            internals[n] = internals[n] / (1 + abs(internals[n]));
+        }
+        for (int n = 0; n < output_number; n++){ // internal -> outputs
+            outputs[n] = output_bias[n];
+            for (int i = 0; i < internal_number; i++){
+                outputs[n] += internals[i]*internal_to_output_weight[n][i]; // This feels like it might be the wrong way round
+            }
+            outputs[n] = outputs[n] / (1 + abs(outputs[n]));
+        }
+    }
+};
+
+
+class Agent{
+public:
+    float pos[2];
+    float direction;
+    float energy;
+    float age;
+
+    Network network;
+
+    Agent(float x, float y){
+        this->pos[0] = x;
+        this->pos[1] = y;
+    }
+};
+
 
 const int screen_width = 1000;
 const int screen_height = 1000;
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
-
-
 
 bool running = true;
 bool paused = false;
@@ -38,7 +96,9 @@ void FixedUpdate(){ // Fixed time updater
 
 
 void draw(){
-//    printf("Draw to screen\n");
+    SDL_SetRenderDrawColor(renderer, 0,0,0,255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 }
 
 
@@ -54,6 +114,10 @@ int main(int argc, char **argv) {
     std::thread physicsThread(&FixedUpdate);
 
 
+    std::vector<Agent> prey;
+    std::vector<Agent> predators;
+
+
     while (running) {
         draw();
         SDL_Event event;
@@ -63,9 +127,7 @@ int main(int argc, char **argv) {
                     running = false;
                     break;
                 case SDL_KEYDOWN:
-                    if (event.key.keysym.sym ==SDLK_SPACE){
-                        paused = !paused;
-                    }
+                    if (event.key.keysym.sym ==SDLK_SPACE)paused = !paused;
                     break;
             }
         }
